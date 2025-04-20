a97d67145b191bd2b930cc23672121e6f10c01c9
userwljs <gaozimu_0502@163.com>
2025-04-20 10:42:00 +0800

    Add docstring for AutoName. 为 AutoName 添加文档。

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 
 class AutoName(Enum):
+    """Each Enum item has a value that is the same as its name.
+    每个 Enum 项的值与它的名称相同。"""
     @staticmethod
     def _generate_next_value_(name, start, count, last_values):
         return name
