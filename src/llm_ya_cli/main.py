0bd250df3227998f2086ea75c4718d98bc6b7bb9
userwljs <gaozimu_0502@163.com>
2025-06-21 18:46:26 +0800

    Don't load configuration when importing this as a package. 在以包导入此项目时不加载配置。

--- src/llm_ya_cli/main.py
+++ src/llm_ya_cli/main.py
@@ -2,15 +2,19 @@
 # The LLM CLI is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 # The LLM CLI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License along with the LLM CLI. If not, see <https://www.gnu.org/licenses/>.
-import click
+def main():
+    import click
 
-from .chat import ChatCommand
-from .utils.config import load_config
+    from .chat import ChatCommand
+    from .utils.config import load_config
 
-config = load_config()
+    config = load_config()
 
-cli = click.Group()
-cli.add_command(ChatCommand(config))
+    cli = click.Group()
+    cli.add_command(ChatCommand(config))
+
+    cli()
+
 
 if __name__ == "__main__":
-    cli()
+    main()
